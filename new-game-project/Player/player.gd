01c6f5f8812fe45b881eacd87451296b4bfd3619
SirBarbecuedWaffle <barbecuedwaffle101@gmail.com>
2026-01-08 12:39:40 -0600

    CollisionPrinterYIPPEE

--- new-game-project/Player/player.gd
+++ new-game-project/Player/player.gd
@@ -14,3 +14,9 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0,0.0,delta*100))
 	if Input.is_action_pressed("rot_right"):
 		apply_torque(Vector3(0.0,0.0,delta*-100))
+
+
+func _on_body_entered(body: Node) -> void:
+	print(body.name)
+	print("AAAAAAAAAAAAAAAAAA")
+	
